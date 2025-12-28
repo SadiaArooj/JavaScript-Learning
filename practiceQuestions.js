77d8ddb752a518ba663a9528e6786251c5050dce
SadiaArooj <sadiaarooj2000@gmail.com>
2025-12-28 23:46:06 +0100

    Some practice questions with loop, strings

---
+++ practiceQuestions.js
@@ -0,0 +1,28 @@
+// Question 1: Create a game where you start with any game number and then ask the user to keep guessing
+//  a random number between 1-25 until the user guessed the right number. 
+
+//let gameNum = 10;
+/*  let userNum;
+
+do {
+  userNum = prompt("Guess the game number between 1 to 25");
+} while (userNum != gameNum);
+
+console.log("Congratulations you guessed the right number");
+ */
+
+/* let userNum = prompt("Enter a number between 1-25");
+
+while(gameNum != userNum) {
+userNum = prompt ("You entered wrong number. Try again");
+}
+console.log("Congratulations. you entered the right number"); */
+
+// Question 2: Prompt the user to enter their full name. Generate a user name for them based on input.
+//  Starting with @ and ending with the length of user name e.g SadiaArooj --> @SadiaArooj 10
+
+let  userName = prompt("Enter you name without spaces");
+let newName = "@" + userName + userName.length ;
+console.log("New Name will become:", newName);
+
+// Question 3: 
