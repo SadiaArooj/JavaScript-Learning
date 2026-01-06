017137d40b5df5a88763d652e7a3095302d79e97
SadiaArooj <sadiaarooj2000@gmail.com>
2026-01-06 23:59:53 +0100

    Connect JS to HTML via ID and implement 1-second interval updates

--- dates.js
+++ dates.js
@@ -1,4 +1,22 @@
-let myDate = new Date();
-console.log(myDate.getTime());
-console.log(myDate.getFullYear());
+const options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+// 1st way by making an updateClock Function and then passing it to setInterval();
+/* function updateClock(){
+a = new Date();  // Get the NEW current time every time the function runs
+let time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds(); // In time format
+let date = a.toLocaleDateString("en-US",options); 
+document.getElementById("time").innerHTML = time + " on " + date; // DOM
+}
+setInterval(updateClock, 1000); */
 
+// 2nd way is by using the setInterval() function itself
+setInterval(() => {
+  a = new Date(); // Get the NEW current time every time the function runs
+  let time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds(); // In time format
+  let date = a.toLocaleDateString("en-US", options);
+  document.getElementById("time").innerHTML = time + " on " + date; // DOM
+}, 1000);
